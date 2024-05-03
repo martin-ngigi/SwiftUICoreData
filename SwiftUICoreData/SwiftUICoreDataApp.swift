996ad14e30aee687e32e142cc7dadec447dabc06
martin-ngigi <martinwainaina001@gmail.com>
2024-05-03 17:48:50 +0300

    Initial commit

---
+++ SwiftUICoreData/SwiftUICoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoreDataApp.swift
+//  SwiftUICoreData
+//
+//  Created by Martin Wainaina on 03/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
